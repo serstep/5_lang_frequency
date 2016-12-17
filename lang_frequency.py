0a99deb096cd998ce3c5d6904c611b10b0643dd3
serstep <myserstep@mail.ru>
2016-12-17 14:24:32 +0200

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,45 @@
-
+#!/usr/bin/python3.4
+import sys
+import re
 
 def load_data(filepath):
-    pass
+    text = ""
+    with open(filepath) as file:
+        text = file.read()
 
+    return text
 
 def get_most_frequent_words(text):
-    pass
+    regexp = "[^\s\d.,-:'\"`;)(?!&]+"
+    corpus = re.findall(regexp, text)
+
+    required_words_number = 10
+    frequency_table = dict()
+
+    for i in corpus:
+        if frequency_table.get(i) is None:
+            frequency_table[i] = 1
+        else:
+            frequency_table[i] += 1
+
+    most_frequent_words = list()
+
+    for i in range(required_words_number):
+        most_frequent_word = max(frequency_table, key=lambda w: frequency_table[w], default=None)
+
+        most_frequent_words.append(most_frequent_word)
+        frequency_table.pop(most_frequent_word)
+
+    return most_frequent_words
 
 
 if __name__ == '__main__':
-    pass
+    text = ""
+    try: 
+        text = load_data(sys.argv[1])
+    except:
+        print("Невозможно прочитать файл.")
+        exit(0)
+
+    for i in get_most_frequent_words(text):
+        print(i)
