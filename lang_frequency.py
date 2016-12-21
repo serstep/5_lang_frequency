76372c611b757c803a00789f94bca3c2658d8362
serstep <myserstep@mail.ru>
2016-12-21 18:36:31 +0200

    Использована коллекция Counter.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,45 +1,28 @@
-#!/usr/bin/python3.4
 import sys
 import re
+from collections import Counter  
+
 
 def load_data(filepath):
-    text = ""
-    with open(filepath) as file:
-        text = file.read()
+    with open(filepath) as file_with_text:
+        text = file_with_text.read()
 
     return text
 
+
 def get_most_frequent_words(text):
     regexp = "[^\s\d.,-:'\"`;)(?!&]+"
-    corpus = re.findall(regexp, text)
-
+    words_list = re.findall(regexp, text)
     required_words_number = 10
-    frequency_table = dict()
-
-    for i in corpus:
-        if frequency_table.get(i) is None:
-            frequency_table[i] = 1
-        else:
-            frequency_table[i] += 1
-
-    most_frequent_words = list()
-
-    for i in range(required_words_number):
-        most_frequent_word = max(frequency_table, key=lambda w: frequency_table[w], default=None)
-
-        most_frequent_words.append(most_frequent_word)
-        frequency_table.pop(most_frequent_word)
-
-    return most_frequent_words
+    return Counter(words_list).most_common(required_words_number)
 
 
 if __name__ == '__main__':
-    text = ""
     try: 
         text = load_data(sys.argv[1])
     except:
-        print("Невозможно прочитать файл.")
-        exit(0)
+        print("Невозможно прочитать текстовый файл файл")
+        exit(1)
 
     for i in get_most_frequent_words(text):
-        print(i)
+        print("Слово:", i[0], "Количество:", i[1])
